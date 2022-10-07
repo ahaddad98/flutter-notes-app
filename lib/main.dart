cc4c5475aedeaca29efc6de6904d01302553a106
Amine Haddad <aminehaddad@Amines-Mac-mini.local>
2022-10-07 10:58:12 +0100

    add registre part

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_1/views/login_view.dart';
+import 'package:flutter_application_1/views/register_view.dart';
 import 'firebase_options.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'My app', // used by the OS task switcher
-      home: LoginView(),
+      home: Registre(),
     ),
   );
 }
 
-class LoginView extends StatefulWidget {
-  const LoginView({Key? key}) : super(key: key);
-
-  @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class Registre extends StatefulWidget {
-  const Registre({super.key});
-
-  @override
-  State<Registre> createState() => _RegistreState();
-}
-
-class _RegistreState extends State<Registre> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class _LoginViewState extends State<LoginView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('To register'),
-        ),
-        body: FutureBuilder(
-          future: Firebase.initializeApp(
-              options: DefaultFirebaseOptions.currentPlatform),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return Column(
-                  children: [
-                    TextField(
-                      controller: _email,
-                      decoration:
-                          const InputDecoration(hintText: 'Input Email'),
-                    ),
-                    TextField(
-                      controller: _password,
-                      decoration:
-                          const InputDecoration(hintText: 'Input Password'),
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-
-                        final usercredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print(usercredential);
-                      },
-                      child: Text('Register'),
-                    ),
-                  ],
-                );
-                default:
-                  return const Text('Loading');
-            };
-          },
-        ));
-    ;
-  }
-}
